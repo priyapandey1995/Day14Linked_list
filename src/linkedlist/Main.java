2c9f746a62cc266615ea9d78233b4b61e45e71a6
priya <priyapandey8884.blr@gmail.com>
2022-07-13 18:22:19 +0530

    Added:to append 30 and 70 to 56 in linkedlist

--- src/linkedlist/Main.java
+++ src/linkedlist/Main.java
@@ -5,14 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 		/**
 		 * object of class linkedlist created to invoke the methods
-		 	to add values using methods
+		 	firstly,56 is added and then append 30 and then 70
 		 	to display the values using methods
 		 	*/
 		toStart();
 		LinkedList list = new LinkedList();
-		list.toPush(70);
-		list.toPush(30);
-		list.toPush(56);
+		list.add(56);
+		list.toAddNodeAtFirst(30);
+		list.toAddNodeAtFirst(70);
 		list.show();
 		
 	}
